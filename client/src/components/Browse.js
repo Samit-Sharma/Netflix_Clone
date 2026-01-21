3c71f4a22fe37bc82a3a8ae2ddf55baa54673125
Samit Sharma <samisharma501@gmail.com>
2026-01-21 18:22:42 +0530

    Fix ESLint errors in Browse component

--- client/src/components/Browse.js
+++ client/src/components/Browse.js
@@ -1,4 +1,6 @@
-import React, { use, useEffect } from 'react'
+
+import { useEffect } from 'react';
+
 import Header from './Header';
 import { useSelector} from "react-redux";
 import { useNavigate } from "react-router-dom";
